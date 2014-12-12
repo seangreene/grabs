2817089caffd1ab8e105349d3169ca5eb704615d
Erik Reyna <erik@erikreyna.com>
2014-12-12 15:11:11 -0500

    guts and gutters, for rohan

--- scripts/10b88bcd.main.js
+++
@@ -1,1 +0,0 @@
-setTimeout(function(){console.log("bananas"),$(window).trigger("resize")},1e3);var container=document.querySelector("#imgz"),msnry;imagesLoaded(container,function(){"use strict";msnry=new Masonry(container)});
