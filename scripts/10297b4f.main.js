e424f050d1e4214fda1a12c1e7902e8abc96a05b
Erik Reyna <erik@erikreyna.com>
2014-12-12 14:16:11 -0500

    imagesloaded again

--- scripts/10297b4f.main.js
+++
@@ -1,1 +0,0 @@
-!function(){$(document).ready(function(){var a=$("#imgz");a.imagesLoaded(function(){a.masonry()})})}();
---
+++ scripts/8b0f11a8.main.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){var a=$("#imgz");a.imagesLoaded(function(){a.masonry()})});
